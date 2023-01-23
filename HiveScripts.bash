f1fb2066a26d8e48ac1a57aa494137401dc85a5d
Pouya <pouya.ataei@ezyvet.com>
2023-01-23 20:11:24 +1300

    created a partitioned table and moved datA

--- HiveScripts.bash
+++ HiveScripts.bash
@@ -1,4 +1,6 @@
-# Downloading  and unzipping the data set
+# --- PART 1 
+
+# Downloading and unzipping the data set
 wget https://video.udacity-data.com/topher/2020/October/5f8f4412_bx-csv-data/bx-csv-data.zip;
 
 unzip bx-csv-data.zip
@@ -57,8 +59,8 @@ TERMINATED BY '|'
 STORED AS TEXTFILE 
 LOCATION '/user/csv-data'
 tblproperties ("skip.header.line.count"="1");
-# Running th necessary queries for business cases
 
+# Running th necessary queries for business cases
 SELECT COUNT(DISTINCT userid) 
 FROM book_db.bx_users 
 WHERE age IS NOT NULL or age != "NULL";
@@ -73,3 +75,75 @@ FROM book_db.bx_books
 GROUP BY author 
 ORDER BY total_books_published DESC 
 LIMIT 5;
+
+# --- PART 2
+
+# Downloading  and unzipping the data set
+wget https://video.udacity-data.com/topher/2020/November/5fabec84_game-reviews.tsv/game-reviews.tsv.gz;
+
+gzip -d game-reviews.tsv.gz 
+
+hdfs dfs -mkdir /user/csv-data/
+
+hdfs dfs -put game-reviews.tsv /user/csv-data/game-reviews.tsv
+
+hive 
+
+# Creating a new database
+CREATE DATABASE demodb;
+
+CREATE EXTERNAL TABLE gameReviews(
+    marketplace string, 
+    customer_id string,
+    review_id string, 
+    product_id string, 
+    product_parent string, 
+    product_title string, 
+    product_category string, 
+    star_rating int, 
+    helpful_votes int, 
+    total_votes int,
+    vine string, 
+    verified_purchase string, 
+    review_headline string,
+    review_body string, 
+    review_date string
+) 
+ROW FORMAT 
+DELIMITED FIELDS 
+TERMINATED BY '|'
+STORED AS TEXTFILE 
+LOCATION '/user/csv-data'
+tblproperties ("skip.header.line.count"="1");
+
+
+CREATE EXTERNAL TABLE gameReviewsPartitioned(
+    marketplace string, 
+    customer_id string,
+    review_id string, 
+    product_id string, 
+    product_parent string, 
+    product_title string, 
+    product_category string, 
+    star_rating int, 
+    helpful_votes int, 
+    total_votes int,
+    vine string, 
+    verified_purchase string, 
+    review_headline string,
+    review_body string, 
+    review_date string
+) 
+PARTITIONED BY (year string)
+ROW FORMAT 
+DELIMITED FIELDS 
+TERMINATED BY '|'
+STORED AS TEXTFILE 
+LOCATION '/user/csv-data'
+tblproperties ("skip.header.line.count"="1");
+
+SET hive.exec.dynamic.partition.mode = nonstrict;
+
+INSERT OVERWRITE TABLE gameReviewsPartitioned 
+PARTITION (year)
+SELECT *, year(review_date) from gameReviews;
